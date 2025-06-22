07e1f7841f6ddaed1ae6264ac0b6e29643a372bb
Vũ Đức Nhâm <vuducnham1123@gmail.com>
2025-06-22 21:04:12 +0700

    Lần đầu commit sau khi clone

--- test-cici/Program.cs
+++ test-cici/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arrInt = { 4, 5, 8, 3, 5, 9, 2, 1, 6, 7 };
+			int max = 0;
+			for (int i = 0; i < arrInt.Length; i++)
+			{
+				if (arrInt[i] > max)
+				{
+					max = arrInt[i];
+				}
+			}
+			Console.WriteLine("max là " + max);
+		}
+	}
+}
